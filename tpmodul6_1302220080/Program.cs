6dd96f2d57182e2995ce9a08c52e74128f2b8102
nabielascar <ascarnabiel@gmail.com>
2024-03-25 02:11:27 +0700

    menambahkan class SayaTubeVidio

--- tpmodul6_1302220080/Program.cs
+++ tpmodul6_1302220080/Program.cs
@@ -2,9 +2,40 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public class SayaTubeVideo
         {
-            Console.WriteLine("Hello, World!");
+            private int id;
+            private string title;
+            private int playCount;
+
+            public SayaTubeVideo(String title)
+            {
+                this.title = title;
+                Random rand = new Random();
+                int min = 100000;
+                int max = 999999;
+                this.id = rand.Next(min, max + 1);
+                this.playCount = 0;
+            }
+
+            public void IncreasePlayCount(int playCount)
+            {
+                this.playCount = this.playCount + playCount;
+            }
+
+            public void PrintVideoDetails()
+            {
+                Console.WriteLine($"ID Video : {id}");
+                Console.WriteLine($"Title Video : {title}");
+                Console.WriteLine($"Jumlah Tontonan : {playCount}");
+            }
+
+            static void Main(string[] args)
+            {
+                string judul = "Tutorial Design By Contract - Nabiel Ascar";
+                SayaTubeVideo sayaTubeVideo = new SayaTubeVideo(judul);
+                sayaTubeVideo.PrintVideoDetails();
+            }
         }
     }
 }
